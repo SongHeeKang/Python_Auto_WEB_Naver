c79aecbad3bc0c3780625ae908c402f2e3a0297e
heekangsong <heekangsong@gmail.com>
2024-06-30 16:57:25 +0900

    update

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 BASE_URL = "https://www.naver.com/"
 PASS_SCREENSHOT = True
 WAIT_TIME = 10
-SELENIUM_GRID_URL = "http://localhost:4444/wd/hub"
-# SELENIUM_GRID_URL = "http://172.17.0.3:4444/wd/hub"
+# SELENIUM_GRID_URL = "http://localhost:4444/wd/hub"
+SELENIUM_GRID_URL = "http://172.17.0.6:4444/wd/hub"
 REMOTE_RUN = True
